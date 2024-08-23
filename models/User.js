3896fc761191ab6c7e06aac35b8488e3626655ac
091-shivani <117371884+091-shivani@users.noreply.github.com>
2024-08-23 20:52:41 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')     //search mongoose schema
+const { Schema } =mongoose;              //destructing in js   //call back funtionality in js
+const UserSchema = new Schema({         //read about models 
+    fullname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    phonenumber:{
+        type: String,
+        required: true
+    },
+    collegename:{
+        type: String,
+        required: true
+    },
+    rollnumber:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+ module.exports = mongoose.model('User', UserSchema)     //whenever it is used, user named collection will be created inside your db                                     
